c994869c7c5941ecc2002a38c7073faf56bfd8d5
Hadi Tadayyon <ahoo_hadi@yahoo.com>
2019-07-26 12:02:53 +0430

    update

--- pcProgram/src/sample/Controller.java
+++ pcProgram/src/sample/Controller.java
@@ -3,37 +3,47 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import org.java_websocket.server.WebSocketServer;
 
+import javax.swing.*;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
+import java.net.*;
+import java.util.Enumeration;
 
 public class Controller extends Thread {
     @FXML
     Label myIp;
+    @FXML
+    TextField pcIp;
 
-    String host ="localhost";
-    int port = 8887;
-    WebSocketServer server = new wsSwerver(new InetSocketAddress(host,port));
+//    String host = pcIp.getText();
+//    int port = 8887;
+//    WebSocketServer server = new wsSwerver(new InetSocketAddress(host,port));
 
     @FXML
-    public void initialize() throws UnknownHostException {
-        InetAddress localHost= InetAddress.getLocalHost();
-        System.out.println("system ip is a :"+(localHost.getHostAddress()).trim());
-        myIp.setText((localHost.getHostAddress()).trim() );
+    public void initialize() throws UnknownHostException, SocketException {
+//        InetAddress localHost1= InetAddress.getLocalHost();
+//        InetAddress[] localHost= InetAddress.getAllByName(localHost1.getHostName());
+//        myIp.setText((localHost1.getHostAddress()).trim() );
+//        for (int i = 0; i < localHost.length ;i++){
+//            System.out.println("system ip is a :"+(localHost[i].getHostAddress()).trim());
+//            System.out.println(localHost[1].getHostName());
+//
+//        }
     }
 
     @FXML
-    public void starts(){
-
-        server.start();
+    public void starts() {
+        Main.getInstance().getMwWsSwerver().broadcast("hello");
+//        server.start();
         System.out.println("server is runing");
+//        myIp.setText("server address is:"+pcIp.getText());
     }
+
     @FXML
     public void stops() throws IOException, InterruptedException {
-        server.stop();
+//        server.stop();
         System.out.println("server is stoped");
     }
 }
--- pcProgram/src/sample/Main.java
+++ pcProgram/src/sample/Main.java
@@ -6,10 +6,32 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.InetSocketAddress;
+
 public class Main extends Application {
 
+    private static Main instance;
+    private wsSwerver mwWsSwerver;
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    public Main() {
+        instance = this;
+    }
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void init() throws Exception {
+        super.init();
+        String host = "localhost";
+        int port = 8887;
+        mwWsSwerver = new wsSwerver(new InetSocketAddress(host, port));
+       new Thread(() -> mwWsSwerver.run()).start();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 892, 595));
@@ -19,5 +41,9 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public wsSwerver getMwWsSwerver() {
+        return mwWsSwerver;
     }
 }
